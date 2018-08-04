1a0dfb3b1ad49ebd1158fcf571c31929470609a0
riley Chadwick <bangfireball@yahoo.com>
2018-08-03 23:05:59 -0400

    Initial Commit. Setting up project structure with some assets.

---
+++ Project 2047/Assets/missile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class missile : MonoBehaviour {
+
+    private Transform targetPos; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2.MoveTowards(this.transform, this.transform, )
+	}
+}
